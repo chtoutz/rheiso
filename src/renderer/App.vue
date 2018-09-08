01c8fc9d6525a9a4b3d2c545c9810ec3e700448e
Unknown <valentin.stutz@protonmail.com>
2018-09-09 00:07:04 +0200

    Add core electron structure
    From https://github.com/SimulatedGREG/electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'rheiso'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
