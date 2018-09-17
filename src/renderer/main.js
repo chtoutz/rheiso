e1cbd0a05ed090bd45f80b19011bf0b0be6065a7
Unknown <valentin.stutz@protonmail.com>
2018-09-17 20:13:25 +0200

    Implement notifications globally
    Usage in any component :
    
    ```
    let notification = {
          message: 'Success lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit',
          type: 'primary',
          direction: 'Down',
          duration: 2000
        }
    
        this.$openNotification(notification)
    ```

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -7,8 +7,32 @@ import store from './store'
 
 import './assets/styles.sass'
 
+import Notification from '@/components/Layout/Notification'
+
+const NotificationComponent = Vue.extend(Notification)
+
+const openNotification = (propsData = {
+  title: '',
+  message: '',
+  type: 'primary',
+  direction: 'Down',
+  duration: 4500,
+  container: '.notifications'
+}) => {
+  return new NotificationComponent({
+    el: document.createElement('div'),
+    propsData
+  })
+}
+
+// Init db files loading from DataBase VueX store
+store.commit('initDatabase')
+// Init notifications from Notifications VueX store
+// store.commit('initNotifications')
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
+Vue.openNotification = Vue.prototype.$openNotification = openNotification
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
